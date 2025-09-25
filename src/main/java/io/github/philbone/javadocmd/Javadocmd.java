61c7473380a5ae52f087e4ac84f1f0e67a365e98
Felipe M <philbone@focused.cl>
2025-09-25 13:46:41 -0300

    Commit inicial

---
+++ src/main/java/io/github/philbone/javadocmd/Javadocmd.java
@@ -0,0 +1,14 @@
+
+
+package io.github.philbone.javadocmd;
+
+/**
+ *
+ * @author felipe
+ */
+public class Javadocmd {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
